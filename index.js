8892dfdc6ac1567770850a70adeb1ad53771eaa6
riyoneri <lionkaneza@gmail.com>
2023-03-08 01:06:33 +0200

    Read file datas

--- index.js
+++ index.js
@@ -1,77 +1,34 @@
 const fs = require('fs')
 
-// const readLine = require('readline')
-// const fileStream = fs.createReadStream("file.txt")
-
-// const rl = readLine.createInterface({
-//     input: fileStream,
-//     crlfDelay: Infinity
-// })
-
-// let lineCounter = 0;
-// let twoNumbersLine;
-
-// let testCases;
-// let i = 1;
-
-// let squareNumbers;
-// let targetedSquares;
-
-// let startingTargetedSquare;
-// let startingTargetedSquareIndex = 0;
-
-// let startedLines = false;
-
-// rl.on("line", line => {
-//     lineCounter += 1;
-//     startedLines = false;
-
-//     if (lineCounter == 1) {
-//         testCases = +line
-//     }
-
-//     if (line.length === 3) {
-//         twoNumbersLine = lineCounter;
-
-//         squareNumbers = +line[0]
-//         targetedSquares = +line[2]
-//     }
-
-//     if (lineCounter - twoNumbersLine === 1) {
-//         startingTargetedSquare = line.split(" ").map(item => item = +item)
-//         startingTargetedSquareIndex = lineCounter
-//     }
-
-//     if (startedLines) {
-
-
-//         // while (i < targetedSquares) {
-//         //     console.log(line)
-//         //     i += 1;
-//         // }
-//     }
-// })
-
 const allLines = fs.readFileSync('file.txt', 'utf-8').split(/\r?\n/)
 
 let lineCounter = 0;
-let twoNumbersLine;
 
 let testCases = +allLines[0];
-let i = 1;
 
-let squareNumbers = allLines.filter(line => line.length === 3);
-let targetedSquares;
+let twoNumberedLines = allLines.filter((line, i) => {
+    if (allLines[i].length === 3 && allLines[i + 1].length === 5) {
+        return line
+    }
+});
 
-let startingTargetedSquare;
-let startingTargetedSquareIndex = 0;
+let firstTargettedSquares = allLines.filter((line, i) => {
+    if (allLines[i].length === 5 && allLines[i - 1].length === 3) {
+        return line
+    }
+})
 
-let startedLines = false;
+let targetedSquares = allLines.filter((line, i) => {
+    if ((allLines[i].length === 5 && allLines[i - 1].length === 5) || (allLines[i].length === 5 && allLines[i + 1].length === 3)) {
+        return line
+    }
+})
 
-for (let i = 1; i <= testCases; i++) {
-    targetedSquares = allLines.filter((line, i) => {
-        if (line.length === 4 && line.length === 3) {
-            
-        }
-    })
+
+for (let i = 0; i < testCases; i++) {
+
+    // console.log(twoNumberedLines[i])
+    for (let j = 1; j <= +twoNumberedLines[i].split(" ")[1]; j++) {
+        console.log(+twoNumberedLines[i].split(" ")[1])
+    }
 }
